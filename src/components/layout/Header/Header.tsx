10250d5dac565cba77f6b79d5484e9cad411df7b
Estebans441 <salazaraesteban@javeriana.edu.co>
2025-10-23 13:01:12 -0500

    feat: update logo

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div className={styles.brand}>In<u>Vendor</u>y</div>
+        <div className={styles.brand}>
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={250}
+            height={250 / 4}
+            className={styles.logo}
+            priority
+          />
+        </div>
       </div>
     </header>
   );
