c28e0367d2a98bad48b630cbedd51db41a071f03
Esteban Salazar <salazaraesteban@javeriana.edu.co>
2025-10-23 02:04:58 -0500

    feat: menu layout

---
+++ app/providers/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductsPage() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-4">Providers</h1>
+    </section>
+  );
+}
--- src/components/layout/Menu/Menu.tsx
+++ src/components/layout/Menu/Menu.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Menu.module.css";
+import { useState } from "react";
+
+import { Box, Truck, User } from 'lucide-react';
 
 export default function Menu() {
+  const [selectedItem, setSelectedItem] = useState<string>("");
 
   const menuItems = [
-    { href: "/", label: "Home" },
-    { href: "/products", label: "Products" },
+    { href: "/products", label: "Products", icon: <Box /> },
+    { href: "/providers", label: "Providers", icon: <Truck /> },
   ];
 
   return (
     <div className={styles.menuContainer}>
       <ul className={styles.menuList}>
         {menuItems.map((item) => (
-          <li key={item.href} className={styles.menuItem}>
-            <Link href={item.href}>
-              {item.label}
+          <li key={item.href} className={`${styles.menuItem} ${selectedItem === item.href ? styles.selectedMenuItem : ""}`}>
+            <Link 
+                className={styles.option}
+                href={item.href} 
+                onClick={() => setSelectedItem(item.href)}
+              >
+              <div className={`${styles.iconWrapper}`}>
+                {item.icon}
+                {item.label}
+              </div>
             </Link>
           </li>
         ))}
